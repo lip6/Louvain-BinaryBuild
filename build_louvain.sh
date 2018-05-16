b1420777c138aa6532812499409e4de9cc472cd0
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-05-16 13:48:17 +0200

    build script (pretty basic...)

---
+++ build_louvain.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+set -x
+
+tar zxf louvain-generic.tar.gz
+
+cd louvain-generic/
+
+make 
+
+tar cvzf louvain_linux.tar.gz convert louvain matrix hierarchy README.txt
+
+mv louvain_linux.tar.gz ../website/
+
+cd ..
